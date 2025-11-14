63e50a878444a91233d3b23784fdff8878ceacd1
DiegoGp227 <diego.gp227@gmail.com>
2025-11-14 10:29:48 -0500

    [ init ] docker integrated

--- back/routes/index.routes.ts
+++ back/routes/index.routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import dbCheck from "../controllers/test/test.js";
-import signup from "../controllers/auth/signup.controller.js";
-import login from "../controllers/auth/login.contoller.js";
+// import login from "../controllers/auth/login.contoller.js";
 
 export const router = Router();
 
 // Auth routes
-router.post("/api/signup", signup);
-router.post("/api/login", login);
+// router.post("/api/signup", signup);
+// router.post("/api/login", login);
 
 // Test routes
 router.get("/api/db", dbCheck);
