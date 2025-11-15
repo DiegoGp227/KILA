82e27e438dea2ed24a0de9d68c16a165847eb527
DiegoGp227 <diego.gp227@gmail.com>
2025-11-14 22:21:02 -0500

    [ feat ] steps sisem integrated

--- front/app/components/organism/StepsSistem.tsx
+++ front/app/components/organism/StepsSistem.tsx
@@ -5,7 +5,7 @@ interface IStepsSistem {
 export default function StepsSistem({ StepActive }: IStepsSistem) {
   return (
     <div className="flex justify-center items-center gap-8 my-8">
-      <div className="step active">
+      <div className={`step ${StepActive === 1 ? "active" : ""}`}>
         <div className="step-number">1</div>
         <div className="step-label">Cargar Factura</div>
       </div>
@@ -19,7 +19,7 @@ export default function StepsSistem({ StepActive }: IStepsSistem) {
         }}
       ></div>
 
-      <div className="step">
+      <div className={`step ${StepActive === 2 ? "active" : ""}`}>
         <div className="step-number">2</div>
         <div className="step-label">Validando</div>
       </div>
@@ -32,8 +32,8 @@ export default function StepsSistem({ StepActive }: IStepsSistem) {
           margin: "0 -1rem",
         }}
       ></div>
-      
-      <div className="step">
+
+      <div className={`step ${StepActive === 3 ? "active" : ""}`}>
         <div className="step-number">3</div>
         <div className="step-label">Resultados</div>
       </div>
