bfe50d90b7562efcf857ee41858ed3b855a83940
DiegoGp227 <diego.gp227@gmail.com>
2025-11-29 20:58:39 -0500

    [ feat ] signup endpoint integrated

--- back/routes/index.routes.ts
+++ back/routes/index.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import dbCheck from "../controllers/test/test.js";
+import { signup } from "../controllers/auth/auth.controllers.js";
 
 export const router = Router();
+
+//Auth routes
+router.get("/api/signup", signup);
 
 // Test routes
 router.get("/api/db", dbCheck);
